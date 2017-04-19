23ffba84b4ec5da99a5563690b186814459084af
joelakwes <joelakwes@gmail.com>
2017-04-19 01:32:52 +0100

    Updated folder structure

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict"
+
+var express = require("express");
+
+var app = express();
+var port = 3000;
+
+app.use(express.static("assets"))
+
+app.get("/", function (request, response) {
+  response.send('test Pages')
+})
+
+app.listen(port, function () {
+  console.log('App running')
+})
