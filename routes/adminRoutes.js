3c54845ca6bb8a0555df5a3973fcbc85cd04e672
joelakwes <joelakwes@gmail.com>
2017-04-19 18:08:40 +0100

    renamed view files to ejs

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,43 +1,72 @@
 var express = require("express");
 var adminRouter = express.Router();
-// var mongodb= require("mongodb").MongoClient;
-
-var books = [
-    {
-        title: "Things Fall Apart",
-        category: "Nigerian",
-        author: "Chinua Achebe",
-        available: true
-    },
-    {
-        title: "The God's are not to be blamed",
-        category: "Thriller",
-        author: "Ola Rotimi",
-        available: false
-    },
-    {
-        title: "Emeka",
-        category: "Biography",
-        author: "Author Name",
-        available: false
-    }
-]
+var mongojs = require("mongojs");
+var logic = require("../functions");
+var db = mongojs("mongodb://libUser:qwerty1234@ds031551.mlab.com:31551/library", ["books"]);
 
 
 
 adminRouter.route("/").get(function(request, response){
-    response.render("login.html");
+    if(logic.isLoggedIn()){
+        response.render("addBook.html");
+    } else {
+        response.render("login.html");
+    }
 });
-adminRouter.route("/add-book").get(function(request, response){
-    // var dbURL = "mongodb://localhost:27017/libraryapp";
-    // mongodb.connect(dbURL, function(error, db){
-    //     var bookBollection = db.collection("books");
-    //     collection.insertMany(books, function(err, results){
-    //         res.send(results);
-    //         db.close();
-    //     })
-    // });
-    // response.send("Inserting books");
+
+
+// Add Book
+adminRouter.route("/add-book").post(function(request, response){
+    var book = request.body;
+    if(!book.title || book.available){
+        response.status(400);
+        response.json({
+            "error": "Enter Book title and availability status"
+        })
+    } else {
+        db.books.save(book, function(err, book){
+            if(err){
+                response.send(err);
+            }
+            response.json(task);
+        })
+    }
+});
+
+// Delete Book
+adminRouter.route("/delete/:id").delete(function(request, response){
+    db.books.remove({_id: mongojs.ObjectId(request.params.id)} , function(error, book){
+        if(error){
+        // response.render("error.html");
+        }
+        response.json(book);
+    })
+});
+
+// Update books
+adminRouter.route("/update/:id").put(function(request, response){
+    var book = request.body;
+    var updateBook = {};
+
+    if(book.title && book.available){
+        updateBook.title = book.title,
+        updateBook.category = book.category,
+        updateBook.author = book.author,
+        updateBook.available = book.available
+
+        db.books.update({_id: mongojs.ObjectId(request.params.id)}, updateBook, {}, function(error, book){
+            if(error){
+            // response.render("error.html");
+            }
+            response.json(book);
+        })
+    } else {
+        response.status(400);
+        response.json({
+            "error": "Bad Data"
+        })
+    }
+
 });
 
 module.exports = adminRouter;
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var bookRouter = express.Router();
 var mongojs = require("mongojs");
+var db = mongojs("mongodb://libUser:qwerty1234@ds031551.mlab.com:31551/library", ["books"]);
 
 
 bookRouter.route("/").get(function(request, response){
-    // response.render("books.html");
+    db.books.find(function(error, books){
+        if(error){
+        // response.render("error.html");
+        }
+        response.json(books);
+    })
+});
+
+// For single Book
+bookRouter.route("/:id").get(function(request, response){
+    db.books.findOne({_id: mongojs.ObjectId(request.params.id)} , function(error, book){
+        console.log(book);
+        if(error){
+        // response.render("error.html");
+        }
+        response.render("singleBook.ejs", {book: book});
+    })
 });
 
 module.exports = bookRouter;
