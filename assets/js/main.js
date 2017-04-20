d23311d988809b1d6d4afd32cdb9444b4b54e516
joelakwes <joelakwes@gmail.com>
2017-04-20 18:44:29 +0100

    Edited main.js to clear form when a book is added

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,19 +39,28 @@ $(document).ready(function(){
                 data: data,
                 contentType: "application/json; charset=utf-8",
                 success: function(msg) {
+                    // Check if an div with class alert already exists in the dom and remove the class
                     if(alertWrapper.hasClass("alert")) {
                         alertWrapper.attr("class","");
                     }
+                    // Check response and display alert
                     if(msg && msg.length > 5){
                         alertWrapper.addClass("alert alert-success").text("Successfully Added");
+                        // Check if the form processed is the remove book form and remove the book entry from the dom
                         if(form.attr("id") == "deleteBook") {
+                            //get the table Id based on the url passed
                             remTableId = url.substr(url.lastIndexOf('/') + 1);
                             remTable = $('table.manageBooks tr#'+remTableId);
                             remTable.fadeOut("slow", function(){
                                 remTable.remove();
                             })
-                        }
-                        
+                        } else if(form.attr("id") == "add-book" || form.attr("id") == "add-category") {
+                            // Clear all form inputs
+                            $(':input', form)
+                            .not(':button, :submit, :reset, :hidden')
+                            .val('')
+                            .removeAttr('checked')
+                            .removeAttr('selected');                        }
                     } else {
                         alertWrapper.addClass("alert alert-danger").text(msg.error);
                     }
